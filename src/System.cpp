31bfb1f403916326527ac312261269f475b6b201
rought <rought@list.ru>
2021-08-19 14:55:10 +0300

    перенесены функции генерации в отдельные файлы

--- src/System.cpp
+++ src/System.cpp
@@ -14,3 +14,16 @@ int random(int a, int b)
 {
 	return (a + rand() % int(b - a + 1));
 }
+
+int Temp(int j,unsigned int Width)
+{
+	if (j < Width / 2)
+	{
+		return (160 * j / Width - 50 + random(-5, 5));
+	}
+	else
+	{
+		return (-(160 * j / int(Width)) + 110 + random(-5, 5));
+	}
+
+}
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,14 +1,10 @@
 #include "ofApp.h"
 #include<cstdio>
 #include<ctime>
-#include "System.cpp"
 #include "BIOMS.cpp"
+#include "DiamondSquare.h"
 unsigned const int Width = 1024;
-
-
 int waterLvl = 0;
-int boundVal = 0;
-unsigned int roudness = 1;
 int**HeightMap;//нужно удаление массива или создать класс карты и все атрибуты точек держать в этом классе. на каждую точку приходиться параметр высоты, владжности, температруы, расстояни до воды исцева открашивания
 int Temperature[Width][Width];
 int Wet[Width][Width];
@@ -16,118 +12,8 @@ int persent = 0;
 double t;
 using namespace std;
 
-int Temp(int j)
-{
-	if (j < Width / 2)
-	{
-		return (160 * j / Width - 50 + random(-5, 5));
-	}
-	else
-	{
-		return (-(160 * j / int(Width)) + 110 + random(-5, 5));
-	}
 
-}
-/// <DIAMOND SQUARE>
 
-void squareStep(int** Array, int x, int y, int reach, unsigned int Width)
-{
-	int count = 0;
-	float avg = 0.0f;
-	if (edgeCondition(x, y, reach, Width))
-	{
-		Array[x][y] = boundVal;
-		return;
-	}
-	if (x - reach >= 0 && y - reach >= 0)
-	{
-		avg += Array[x - reach][y - reach];
-		count++;
-	}
-	if (x - reach >= 0 && y + reach < Width)
-	{
-		avg += Array[x - reach][y + reach];
-		count++;
-	}
-	if (x + reach < Width && y - reach >= 0)
-	{
-		avg += Array[x + reach][y - reach];
-		count++;
-	}
-	if (x + reach < Width && y + reach < Width)
-	{
-		avg += Array[x + reach][y + reach];
-		count++;
-	}
-	avg += random(int(-roudness * reach), int(roudness * reach));
-	avg /= count;
-	if (avg < 0)
-		avg = 0;
-	else
-	Array[x][y] = round(avg);
-}
-void diamondStep(int** Array, int x, int y, int reach, unsigned int Width)
-{
-	int count = 0;
-	float avg = 0.0f;
-	if (edgeCondition(x, y, reach, Width))
-	{
-		Array[x][y] = boundVal;
-		return;
-	}
-
-	{
-		if (x - reach >= 0)
-		{
-			avg += Array[x - reach][y];
-			count++;
-		}
-		if (x + reach < Width)
-		{
-			avg += Array[x + reach][y];
-			count++;
-		}
-		if (y - reach >= 0)
-		{
-			avg += Array[x][y - reach];
-			count++;
-		}
-		if (y + reach < Width)
-		{
-			avg += Array[x][y + reach];
-			count++;
-		}
-		avg += random(-roudness * reach, roudness * reach);
-		avg /= count;
-		if (avg < 0)
-			avg = 0;
-		else
-		Array[x][y] = (int)avg;
-	}
-}
-void diamondSquare(int** Array, int size, unsigned int Width)
-{
-	int half = size / 2;
-	if (half < 1)
-		return;
-	for (int y = half; y < Width; y += size)
-		for (int x = half; x < Width; x += size)
-			squareStep(Array, x % Width, y % Width, half, Width);
-	int col = 0;
-	for (int x = 0; x < Width; x += half)
-	{
-		col++;
-		//If this is an odd column.
-		if (col % 2 == 1)
-			for (int z = half; z < Width; z += size)
-				diamondStep(Array, x % Width, z % Width, half, Width);
-		else
-			for (int z = 0; z < Width; z += size)
-				diamondStep(Array, x % Width, z % Width, half, Width);
-	}
-	diamondSquare(Array, size / 2, Width);
-}
-/// </DIAMOND SQUARE>
 void ofApp::setup(){
 	srand(time(0));
 	
@@ -246,7 +132,14 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+	if (key == 'd')//удаление массива высот пперед выходом
+	{
+		cout << "key pressed" << endl;
+		for (register int i = 0; i < Width; i++)
+			delete HeightMap[i];
 
+		delete[]HeightMap;
+	}
 }
 
 //--------------------------------------------------------------
