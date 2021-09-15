e4ed1101813fc044f45b107b8b9c181641aa99c7
rought <rought@list.ru>
2021-09-15 16:08:23 +0500

    добавлена матрица цветов и написан примитив для раскраски карты

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -20,34 +20,40 @@ void ofApp::setup(){
 	Map.Scaling(255);
 	cout << "height in range: " << 0 << " - " << Map.Max()<< endl;
 	Map.WaterLvl_setting(24);
-
+	/// <set map color>
+	
+	
 	for (int i = 0; i < Width; i++)
 		for (int j = 0; j < Width; j++)
 		{
 
 			if ((Map(i, j) == 0))
 				color(i,j)=ofColor(0, 0, 128);
-			else/*
-				if ((HeightMap[i][j] >= waterLvl) && (HeightMap[i][j] <= 1 + waterLvl))
-					ofSetColor(218, 165, 32);//песок
+			else
+				if ((Map(i,j) >= 0) && (Map(i, j) <= 10))
+					color(i,j)=ofColor(218, 165, 32);//песок
 
 				else
-					if (HeightMap[i][j] > 170)
-						ofSetColor(255, 250, 250);
+					if (Map(i, j) > 170)
+						color(i, j) = ofColor(255, 250, 250);//снежная вершина
 					else
-						if ((HeightMap[i][j] < 40) && (HeightMap[i][j] > 1 + waterLvl))
-							ofSetColor(144, 238, 144);
+						if ((Map(i, j) < 50) && (Map(i, j) > 10))//типа лесочек
+							color(i, j) = ofColor(34, 177, 34);
 						else
-							if (HeightMap[i][j] >= 40 && HeightMap[i][j] <= 80)
-								ofSetColor(205, 179, 139);
+							if (Map(i, j) >= 50 && Map(i, j) <80)//темный лес
+								color(i, j) = ofColor(44, 133,84);
 							else
-								if (HeightMap[i][j] > 80 && HeightMap[i][j] < 170)
-									ofSetColor(139, 139, 141);
+							if (Map(i, j) >= 80 && Map(i, j) <= 110)//горный массив
+								color(i, j) = ofColor(86, 103, 117);
+							else
+								if (Map(i, j) > 110 && Map(i, j) < 170)//серые скалы
+									color(i, j) = ofColor(118, 123, 120);
 								else
-								*/
+								
 				color(i,j)=ofColor(Map(i,j), Map(i, j), Map(i, j));
 
 		}
+	/// </summary>
 }//так уменьшение температуры способствует уменьшению осадков но увеличение высоты увеличитает кол во осадков при том что на высоте температура ниже хмхм
 
 
@@ -69,7 +75,6 @@ void ofApp::draw(){
 	{	if (!(i<0 || i>Width))// если камера захватывает территорию без карты высот то там ничего не рисуем
 		for (int j = 0; j < ofGetHeight(); j++)
 		{
-			int h = Map(i, j);
 			if (!(j<0 || j>Width))// если камера захватывает территорию без карты высот то там ничего не рисуем
 			{
 				ofSetColor(color(i, j));
